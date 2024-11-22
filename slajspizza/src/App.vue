e414824510f7ef92684986803491d3c4cb8be3f9
Alicia Karlsson <aliciaxd@live.se>
2024-11-22 11:37:45 +0100

    Co-authored-by: paulinakattilavaara <paulinakattilavaara@users.noreply.github.com>

--- slajspizza/src/App.vue
+++ slajspizza/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from "./components/Header.vue"
 import TheBanner from "./components/TheBanner.vue"
+import Map from "./components/Map.vue"
 </script>
 
 <template>
 <Header />
 <TheBanner />
+<Map />
 </template>
 
 <style>
