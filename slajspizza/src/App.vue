adb1fccf9ef0c3896eacb4c261ffaeb82eea9335
Emil Högberg <emil.hogberg@gmail.com>
2024-11-22 11:46:06 +0100

    Created structure for products with hardcoded data

--- slajspizza/src/App.vue
+++ slajspizza/src/App.vue
@@ -1,17 +1,18 @@
 <script setup>
-import Header from "./components/Header.vue"
-import TheBanner from "./components/TheBanner.vue"
+import Header from './components/Header.vue'
+import TheBanner from './components/TheBanner.vue'
+import Products from './components/Products.vue'
 </script>
 
 <template>
-<Header />
-<TheBanner />
+  <Header />
+  <TheBanner />
+  <Products />
 </template>
 
 <style>
-body{
+body {
   margin: 0;
   padding: 0;
 }
 </style>
-
